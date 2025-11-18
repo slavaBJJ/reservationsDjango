5ccbf26fb3ce0af2608281a16ce72331f37ee37d
Slavic Lisa <slaventi95@gmail.com>
2025-11-18 23:45:34 +0100

    After framework configuration

--- reservations/settings.py
+++ reservations/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+'catalogue',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'reservations.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'reservations',
+	'USER'; 'root',
+	'PASSWORD':",
+	'HOST':'127.0.0.1',
+	'PORT':33O6'
     }
 }
 
