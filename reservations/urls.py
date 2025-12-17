44e1f98ef7d91d2a912bab2e7ecaf891ef579155
Slavic Lisa <slaventi95@gmail.com>
2025-12-18 00:27:00 +0100

    Backend authentification

--- reservations/urls.py
+++ reservations/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('compte/',include('django.contrib.auth.urls')),
     path('catalogue/', include('catalogue.urls')),
+    path( "admin/password_reset/", auth_views.PasswordResetView.as_view( extra_context={"site_header": admin.site.site_header} ), name="admin_password_reset", ),
+    path( "admin/password_reset/done/", auth_views.PasswordResetDoneView.as_view( extra_context={"site_header": admin.site.site_header} ), name="password_reset_done", ),
+    path( "reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view( extra_context={"site_header": admin.site.site_header} ), name="password_reset_confirm", ),
+    path( "reset/done/", auth_views.PasswordResetCompleteView.as_view( extra_context={"site_header": admin.site.site_header} ), name="password_reset_complete",),
     path('admin/', admin.site.urls),
+
 ]
 admin.site.index_title = "Projet Réservations"
 admin.site.index_header = "Projet Réservation HEADER"
